7fd2306eb27650981124650d3d9295207c066130
Aaron Spear <aspear@vmware.com>
2017-05-16 11:56:12 -0600

    restructuring project

--- airwatch/build-airwatch.sh
+++ airwatch/build-airwatch.sh
@@ -28,12 +28,6 @@ else
     mkdir -p ${SCRIPT_DIR}/tools
     pushd ${SCRIPT_DIR}/tools
 
-    # FIXME eventually when we pick up the other change, switch to
-    # using the apixlocal in the tools distribution.
-    #mkdir apixlocal
-    #cd apixlocal
-    #unzip ../../apixlocal.zip
-	
     unzip ${SCRIPT_DIR}/api-explorer-tools-*.zip
 
     popd
@@ -52,34 +46,22 @@ echo "overwriting config with local config"
 cp -f ${SCRIPT_DIR}/config.js .
 
 # run the tool to stage the swagger json files from the 
-# ${SCRIPT_DIR}/swagger-vra directory to the local/swagger
+# ${SCRIPT_DIR}/swagger directory to the local/swagger
 # directory abbreviating the descriptions and then also 
 # generating overview HTML next to the json files.  when it does
 # this it generates an "overview" resource in the local.json file
 # that has all of the configuration in it
 
-#python ${TOOLS_DIR}/apiFilesToLocalJson.py \
-# --abbreviateDescription \
-# --generateOverviewHtml \
-# --apiPrepend="vRealize Automation " \
-# --productName="vRealize Automation;7.3.0" \
-# --apiVersion="(vRealize Automation 7.3.0)" \
-# --swaggerglob ${SCRIPT_DIR}/swagger-vra/api*.json \
-# --outdir=${OUTPUT_DIR}/local/swagger \
-# --htmlRootDir ${OUTPUT_DIR} \
-# --outfile ${OUTPUT_DIR}/local.json
-
-echo "Mirroring API content from ${APIX_SERVER}"
+echo "Staging local API content"
 python ${TOOLS_DIR}/apixlocal/apixlocal.py \
  --server=${APIX_SERVER} \
  --html_root_dir=${OUTPUT_DIR} \
  --output_file=${OUTPUT_DIR}/local.json  \
  --abbreviateDescription \
  --generateOverviewHtml \
- --apiPrepend="vRealize Automation " \
- --productName="vRealize Automation;7.3.0" \
- --apiVersion="(vRealize Automation 7.3.0)" \
- --swaggerglob ${SCRIPT_DIR}/swagger-vra/api*.json \
+ --apiPrepend="AirWatch " \
+ --productName="AirWatch;1.0.0" \
+ --swaggerglob ${SCRIPT_DIR}/swagger/*.json \
  --swagger_output_dir=${OUTPUT_DIR}/local/swagger \
 
 popd
