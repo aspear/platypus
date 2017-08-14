7c370279d46f6525686c1eac376233a7053fb121
Aaron Spear <aspear@vmware.com>
2017-08-14 16:53:40 -0600

    updated to version 0.0.28
    Signed-off-by: Aaron Spear <aspear@vmware.com>

--- local/build.sh
+++ local/build.sh
@@ -10,7 +10,7 @@ TOOLS_DIR=${SCRIPT_DIR}/tools
 
 APIX_SERVER=https://vdc-repo.vmware.com
 
-export VER="0.0.25"
+export VER="0.0.28"
 APIX_RELEASE_URL=https://github.com/vmware/api-explorer/releases/download/${VER}
 
 if [ ! -f api-explorer-dist-${VER}.zip ]; then
@@ -46,33 +46,23 @@ echo "overwriting config with local config"
 cp -f ${SCRIPT_DIR}/config.js .
 
 # run the tool to stage the swagger json files from the 
-# ${SCRIPT_DIR}/swagger-vra directory to the local/swagger
+# ${SCRIPT_DIR}/swagger directory to the local/swagger
 # directory abbreviating the descriptions and then also 
 # generating overview HTML next to the json files.  when it does
 # this it generates an "overview" resource in the local.json file
 # that has all of the configuration in it
 
-#python ${TOOLS_DIR}/apiFilesToLocalJson.py \
-# --abbreviateDescription \
-# --generateOverviewHtml \
-# --apiPrepend="vRealize Automation " \
-# --productName="vRealize Automation;7.3.0" \
-# --apiVersion="(vRealize Automation 7.3.0)" \
-# --swaggerglob ${SCRIPT_DIR}/swagger-vra/api*.json \
-# --outdir=${OUTPUT_DIR}/local/swagger \
-# --htmlRootDir ${OUTPUT_DIR} \
-# --outfile ${OUTPUT_DIR}/local.json
-
-echo "Mirroring API content from ${APIX_SERVER}"
+echo "staging local vRA API content"
 python ${TOOLS_DIR}/apixlocal/apixlocal.py \
+ stage \
  --server=${APIX_SERVER} \
  --html_root_dir=${OUTPUT_DIR} \
  --output_file=${OUTPUT_DIR}/local.json  \
- --abbreviateDescription \
- --generateOverviewHtml \
- --productName="vRealize Automation;7.3.0" \
- --apiVersion="(vRA 7.3.0)" \
- --swaggerglob ${SCRIPT_DIR}/swagger/api*.json \
+ --abbreviate_description \
+ --generate_overview_html \
+ --product_name="vRealize Automation;7.3.0" \
+ --api_version="(vRA 7.3.0)" \
+ --swagger_glob ${SCRIPT_DIR}/swagger/api*.json \
  --swagger_output_dir=${OUTPUT_DIR}/local/swagger \
 
 popd
