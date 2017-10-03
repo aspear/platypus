b74d69df6b9fce8415a304f93f8c7566dd7449aa
Aaron Spear <aspear@vmware.com>
2017-10-03 16:53:44 -0600

    removed unused build option from apixlocal

--- local/build.sh
+++ local/build.sh
@@ -89,8 +89,6 @@ python ${TOOLS_DIR}/apixlocal/apixlocal.py \
  --server=${APIX_SERVER} \
  --html_root_dir=${OUTPUT_DIR} \
  --output_file=${OUTPUT_DIR}/local.json  \
- --abbreviate_description \
- --generate_overview_html \
  --product_name="VMware Fusion;10.0" \
  --swagger_glob=${SCRIPT_DIR}/swagger/*.json \
  --swagger_output_dir=${OUTPUT_DIR}/local/swagger \
