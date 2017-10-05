101ae51e7899fc819ad56fcb9f6b6774a129f45e
Aaron Spear <aspear@vmware.com>
2017-10-05 15:35:37 -0600

    updated swagger docs from 7.4

--- local/get-vra-docs.sh
+++ local/get-vra-docs.sh
@@ -2,38 +2,40 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+VRA_HOST=cava-pn-209-000.eng.vmware.com
+
 # 5/15/2017 NOTE: these are currently broken, so removed. 
-# https://vcac-be.eng.vmware.com/healthbroker-proxy-server/api/docs/swagger.json;healthbroker-proxy-server
-# https://vcac-be.eng.vmware.com/config-management-service/api/docs/swagger.json;config-management-service
+# https://${VRA_HOST}/healthbroker-proxy-server/api/docs/swagger.json;healthbroker-proxy-server
+# https://${VRA_HOST}/config-management-service/api/docs/swagger.json;config-management-service
 
 VALUES="
-https://vcac-be.eng.vmware.com/advanced-designer-service/api/docs/swagger.json;advanced-designer-service
-https://vcac-be.eng.vmware.com/approval-service/api/docs/swagger.json;approval-service
-https://vcac-be.eng.vmware.com/branding-service/api/docs/swagger.json;branding-service
-https://vcac-be.eng.vmware.com/catalog-service/api/docs/swagger.json;catalog-service
-https://vcac-be.eng.vmware.com/component-registry/api/docs/swagger.json;component-registry
-https://vcac-be.eng.vmware.com/composition-service/api/docs/swagger.json;composition-service
-https://vcac-be.eng.vmware.com/container-service/api/docs/swagger.json;container-service
-https://vcac-be.eng.vmware.com/content-management-service/api/docs/swagger.json;content-management
-https://vcac-be.eng.vmware.com/endpoint-configuration-service/api/docs/swagger.json;endpoint-configuration-service
-https://vcac-be.eng.vmware.com/event-broker-service/api/docs/swagger.json;event-broker-service
-https://vcac-be.eng.vmware.com/forms-service/api/docs/swagger.json;forms-service
-https://vcac-be.eng.vmware.com/iaas-proxy-provider/api/docs/swagger.json;iaas-proxy-provider
-https://vcac-be.eng.vmware.com/identity/api/docs/swagger.json;identity
-https://vcac-be.eng.vmware.com/ipam-service/api/docs/swagger.json;ipam-service
-https://vcac-be.eng.vmware.com/component-registry/api/docs/licensing/swagger.json;licensing-service
-https://vcac-be.eng.vmware.com/management-service/api/docs/swagger.json;management-service
-https://vcac-be.eng.vmware.com/network-service/api/docs/swagger.json;network-service
-https://vcac-be.eng.vmware.com/notification-service/api/docs/swagger.json;notification-service
-https://vcac-be.eng.vmware.com/o11n-gateway-service/api/docs/swagger.json;o11n-gateway-service
-https://vcac-be.eng.vmware.com/placement-service/api/docs/swagger.json;placement-service
-https://vcac-be.eng.vmware.com/component-registry/api/docs/extensibility/swagger.json;plugin-service
-https://vcac-be.eng.vmware.com/portal-service/api/docs/swagger.json;portal-service
-https://vcac-be.eng.vmware.com/properties-service/api/docs/swagger.json;properties-service
-https://vcac-be.eng.vmware.com/reservation-service/api/docs/swagger.json;reservation-service
-https://vcac-be.eng.vmware.com/software-service/api/docs/swagger.json;software-service
-https://vcac-be.eng.vmware.com:443/vco/api/docs/swagger.json;vco
-https://vcac-be.eng.vmware.com/workitem-service/api/docs/swagger.json;workitem-service
+https://${VRA_HOST}/advanced-designer-service/api/docs/swagger.json;advanced-designer-service
+https://${VRA_HOST}/approval-service/api/docs/swagger.json;approval-service
+https://${VRA_HOST}/branding-service/api/docs/swagger.json;branding-service
+https://${VRA_HOST}/catalog-service/api/docs/swagger.json;catalog-service
+https://${VRA_HOST}/component-registry/api/docs/swagger.json;component-registry
+https://${VRA_HOST}/composition-service/api/docs/swagger.json;composition-service
+https://${VRA_HOST}/container-service/api/docs/swagger.json;container-service
+https://${VRA_HOST}/content-management-service/api/docs/swagger.json;content-management
+https://${VRA_HOST}/endpoint-configuration-service/api/docs/swagger.json;endpoint-configuration-service
+https://${VRA_HOST}/event-broker-service/api/docs/swagger.json;event-broker-service
+https://${VRA_HOST}/forms-service/api/docs/swagger.json;forms-service
+https://${VRA_HOST}/iaas-proxy-provider/api/docs/swagger.json;iaas-proxy-provider
+https://${VRA_HOST}/identity/api/docs/swagger.json;identity
+https://${VRA_HOST}/ipam-service/api/docs/swagger.json;ipam-service
+https://${VRA_HOST}/component-registry/api/docs/licensing/swagger.json;licensing-service
+https://${VRA_HOST}/management-service/api/docs/swagger.json;management-service
+https://${VRA_HOST}/network-service/api/docs/swagger.json;network-service
+https://${VRA_HOST}/notification-service/api/docs/swagger.json;notification-service
+https://${VRA_HOST}/o11n-gateway-service/api/docs/swagger.json;o11n-gateway-service
+https://${VRA_HOST}/placement-service/api/docs/swagger.json;placement-service
+https://${VRA_HOST}/component-registry/api/docs/extensibility/swagger.json;plugin-service
+https://${VRA_HOST}/portal-service/api/docs/swagger.json;portal-service
+https://${VRA_HOST}/properties-service/api/docs/swagger.json;properties-service
+https://${VRA_HOST}/reservation-service/api/docs/swagger.json;reservation-service
+https://${VRA_HOST}/software-service/api/docs/swagger.json;software-service
+https://${VRA_HOST}:443/vco/api/docs/swagger.json;vco
+https://${VRA_HOST}/workitem-service/api/docs/swagger.json;workitem-service
 ";
 
 #for VALUE in $VALUES; do
