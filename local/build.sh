23d0af2aebb6aacc371f307d9c75e44a320b97e4
Aaron Spear <aspear@vmware.com>
2018-08-06 15:04:48 -0600

    almost working but not quite yet

--- local/build.sh
+++ local/build.sh
@@ -2,27 +2,14 @@
 
 ###############################################################################
 # Copyright VMware, Inc. 2017. All Rights Reserved.
-# SPDX-License-Identifier: MIT
+# VMware internal, not open source.  (yet)
 #
-# You are free to use this file under the terms of the MIT open source license,
-# see LICENSE in the root of the containing project.
+# This script builds an web server image in the file system of the VMware {code}
+# "Devcenter App" https://gitlab.eng.vmware.com/gtix-tools/vcode-dev-center-app
+# 
+# Read https://gitlab.eng.vmware.com/gtix-tools/vcode-dev-center-app/blob/master/INTEGRATION.md'
+# for instructions on integration.
 #
-# DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
-# EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
-# WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
-# NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-#
-# This script builds an web server image in the file system of VMware API
-# Explorer (https://github.com/vmware/api-explorer), that is customized in this 
-# file with specific API references.  This image can then be served from any 
-# web container (tomcat, apache, NGINX, ...)
-#
-# VALUES THAT MUST BE CHANGED:
-# -At the bottom the apixlocal command run references string values specific
-# to the local APIs, these must be changed for every differnet package.
-# - the config.js file contains string and configuration values that are 
-# specific to a given set of APIs as well and should be customized.
 ###############################################################################
 
 set -x  # fail the script if any command fails
@@ -35,11 +22,9 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # wish.  If not provided, all output is created in this (the script) directory.
 BUILD_DIR=${BUILD_DIR:-${SCRIPT_DIR}}
 
-APIX_SERVER=https://vdc-repo.vmware.com
-
 # the VER variable is the one place to change the particular release of API 
-# explorer.  See https://github.com/vmware/api-explorer/releases for valid 
-# values
+# explorer.  See https://build-artifactory.eng.vmware.com/artifactory/npm/%40vmw/vcode-dev-center-app/-/@vmw
+# for valid values
 export VER="0.1.0"
 
 # -----------------------------------------------------------------------------
@@ -50,17 +35,17 @@ OUTPUT_DIR=${BUILD_DIR}/staging
 TOOLS_DIR=${BUILD_DIR}/tools
 DOWNLOAD_DIR=${BUILD_DIR}/download
 PUBLISH_DIR=${BUILD_DIR}/publish
-WAR_OUTPUT_DIR=${BUILD_DIR}/staging-war
 
 mkdir -p ${DOWNLOAD_DIR}
-mkdir -p ${PUBLISH_DIR}
 
-# download zips of the distribution and tools if not cached locally
 if [ ! -f ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE} ]; then
+    echo "Downloading archives of the distribution to ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE}"
     wget --no-check-certificate ${ARTIFACTORY_URL}/${DEV_CENTER_APP_FILE} --output-document ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE}
     pushd ${DOWNLOAD_DIR}
     tar xfz ${DEV_CENTER_APP_FILE}  #this creates ${DOWNLOAD_DIR}/package/dist and ${DOWNLOAD_DIR}/package/tools
     rm -rf ${TOOLS_DIR}  # if we downloaded new tools, wipe the old ones
+else
+    echo "Using cached copy of ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE}"
 fi
 
 # only stage the tools once
@@ -77,62 +62,44 @@ fi
 # Clean the staging directory if it already exists
 rm -rf ${OUTPUT_DIR}/*
 mkdir -p ${OUTPUT_DIR}
-
-rm -rf ${WAR_OUTPUT_DIR}/*
-mkdir -p ${WAR_OUTPUT_DIR}
-
 pushd ${OUTPUT_DIR}
 
 echo "Extracting dev-center distribution"
 cp -R ${DOWNLOAD_DIR}/package/dist/* .
 
-echo "FIXME exiting early"
-exit 0
-
 echo "Cleaning up unneeded files in distribution"
 rm -fv ./assets/apix-swagger.json
 rm -fv ./assets/dev-center-overview.html
 rm -fv ./assets/sample-exchange-swagger.json
-rm -fv ./assets/swagger-auth.json
-rm -fv ./assets/swagger-complete.json
+#rm -fv ./assets/swagger-auth.json
+#rm -fv ./assets/swagger-complete.json
 rm -fv ./assets/vra-auth.json
-rm -fv ./assets/vra-config.json
+#rm -fv ./assets/vra-config.json
 rm -rfv ./local
 
-# these are the files we will overwrite
+echo "Removing stock configuration files (will be overwritten, but to catch errors we remove them explicitly)"
 rm -fv ./assets/dev-center-config.json
 rm -fv ./assets/apis.json
 
-echo "Overwriting stock apix-config.json with custom config"
+echo "Staging product specific dev-center-config.json as well as apis.json"
 cp -fv ${SCRIPT_DIR}/dev-center-config.json ./assets
 cp -fv ${SCRIPT_DIR}/apis.json ./assets
+cp -fv ${SCRIPT_DIR}/favicon.ico ./    # copy VMware standard icon.  Replace with your products if there is one.
 
-# run the tool to stage the swagger json files from the 
-# ${SCRIPT_DIR}/swagger directory to the local/swagger
-# directory, abbreviating the descriptions and then also taking the 
-# markdown description from the swagger and generating overview HTML
-# next to the json files.  When it does this it generates an "overview"
-# resource in the local.json file that has all of the configuration in it
-#echo "staging local API content"
-#python ${TOOLS_DIR}/apixlocal/apixlocal.py \
-# stage \
-# --product_name="VMware {code}" \
-# --api_version="" \
-# --swagger_glob ${SCRIPT_DIR}/swagger/*.json \
-# --swagger_output_dir ${OUTPUT_DIR}/local/swagger \
-# --html_root_dir ${OUTPUT_DIR} \
-# --output_file ${OUTPUT_DIR}/local.json \
-# --file_name_to_api_uid_properties_file_path=${SCRIPT_DIR}/api-uid-mappings.properties
-WAR_FILE_NAME=dev-center.war
-pushd ${WAR_OUTPUT_DIR}
-echo "Staging war metadata"
-cp -Rv ${SCRIPT_DIR}/war-template/* ${WAR_OUTPUT_DIR}
-mkdir -p ${WAR_OUTPUT_DIR}/web
-cp -Rv ${OUTPUT_DIR}/* ${WAR_OUTPUT_DIR}/web
-echo "Creating war file ${PUBLISH_DIR}/${WAR_FILE_NAME}"
-zip -r ${PUBLISH_DIR}/${WAR_FILE_NAME} *
-popd
 
+# The below commented code is an attempt to create a static war file wrapper for the content
+#WAR_OUTPUT_DIR=${BUILD_DIR}/staging-war
+#rm -rf ${WAR_OUTPUT_DIR}/*
+#mkdir -p ${WAR_OUTPUT_DIR}
+#WAR_FILE_NAME=dev-center.war
+#pushd ${WAR_OUTPUT_DIR}
+#echo "Staging war metadata"
+#cp -Rv ${SCRIPT_DIR}/war-template/* ${WAR_OUTPUT_DIR}
+#mkdir -p ${WAR_OUTPUT_DIR}/web
+#cp -Rv ${OUTPUT_DIR}/* ${WAR_OUTPUT_DIR}/web
+#echo "Creating war file ${PUBLISH_DIR}/${WAR_FILE_NAME}"
+#zip -r ${PUBLISH_DIR}/${WAR_FILE_NAME} *
+#popd
 #echo "Creating war metadata"
 # now create a war file that is simply a wrapper on the image 
 
@@ -148,3 +115,4 @@ popd
 #zip -r ${PUBLISH_DIR}/${WAR_FILE_NAME} *
 
 popd
+echo "DONE!"
