a88e2a458064b9a099cc09ef5e29052c11af5ddb
Aaron Spear <aspear@vmware.com>
2018-08-08 15:29:13 -0600

    improved build files, does path replacement

--- local/build.sh
+++ local/build.sh
@@ -22,6 +22,10 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # wish.  If not provided, all output is created in this (the script) directory.
 BUILD_DIR=${BUILD_DIR:-${SCRIPT_DIR}}
 
+# if your dev-center is not in the server root, you must supply is not in the root (likely)
+# you must supply this path here by setting DEV_CENTER_ROOT_PATH
+DEV_CENTER_ROOT_PATH=${DEV_CENTER_ROOT_PATH:-""}
+
 # the VER variable is the one place to change the particular release of API 
 # explorer.  See https://build-artifactory.eng.vmware.com/artifactory/npm/%40vmw/vcode-dev-center-app/-/@vmw
 # for valid values
@@ -64,9 +68,8 @@ rm -rf ${OUTPUT_DIR}/*
 mkdir -p ${OUTPUT_DIR}
 pushd ${OUTPUT_DIR}
 
-echo "Extracting dev-center distribution"
-cp -R ${DOWNLOAD_DIR}/package/dist/.* .
-cp -R ${DOWNLOAD_DIR}/package/dist/* .
+echo "Staging dev-center distribution"
+cp -R ${DOWNLOAD_DIR}/package/dist/{.[^.],}* .
 
 echo "Cleaning up unneeded files in distribution"
 rm -fv ./assets/apix-swagger.json
@@ -87,33 +90,14 @@ cp -fv ${SCRIPT_DIR}/dev-center-config.json ./assets
 cp -fv ${SCRIPT_DIR}/apis.json ./assets
 cp -fv ${SCRIPT_DIR}/favicon.ico ./    # copy VMware standard icon.  Replace with your products if there is one.
 
+echo "Using DEV_CENTER_ROOT_PATH variable to set base path in generated files to \"${DEV_CENTER_ROOT_PATH}\""
+sed -i "s|<base href=\\\"/\\\">|<base href=\\\"/${DEV_CENTER_ROOT_PATH}\\\">|g" ./index.html
+sed -i "s|/dev-center-app/|/${DEV_CENTER_ROOT_PATH}/|g" ./.htaccess
+sed -i "s|/dev-center-app/|/${DEV_CENTER_ROOT_PATH}/|g" ./server.rewrites
 
-# The below commented code is an attempt to create a static war file wrapper for the content
-#WAR_OUTPUT_DIR=${BUILD_DIR}/staging-war
-#rm -rf ${WAR_OUTPUT_DIR}/*
-#mkdir -p ${WAR_OUTPUT_DIR}
-#WAR_FILE_NAME=dev-center.war
-#pushd ${WAR_OUTPUT_DIR}
-#echo "Staging war metadata"
-#cp -Rv ${SCRIPT_DIR}/war-template/* ${WAR_OUTPUT_DIR}
-#mkdir -p ${WAR_OUTPUT_DIR}/web
-#cp -Rv ${OUTPUT_DIR}/* ${WAR_OUTPUT_DIR}/web
-#echo "Creating war file ${PUBLISH_DIR}/${WAR_FILE_NAME}"
-#zip -r ${PUBLISH_DIR}/${WAR_FILE_NAME} *
-#popd
-#echo "Creating war metadata"
-# now create a war file that is simply a wrapper on the image 
-
-#mkdir -p ${OUTPUT_DIR}/WEB-INF
-#cat > ${OUTPUT_DIR}/WEB-INF/web.xml <<- "EOF"
-#<web-app version="3.0" xmlns="http://java.sun.com/xml/ns/javaee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd">
-#<display-name>VMware {code} Services Dev Center</display-name>
-#<welcome-file-list><welcome-file>index.html</welcome-file></welcome-file-list>
-#</web-app>
-#EOF
-#echo "Creating war file ${PUBLISH_DIR}/${WAR_FILE_NAME}"
-#rm -f ${PUBLISH_DIR}/${WAR_FILE_NAME}
-#zip -r ${PUBLISH_DIR}/${WAR_FILE_NAME} *
+echo "Publishing output zip file ${PUBLISH_DIR}/dev-center.zip"
+mkdir -p ${PUBLISH_DIR}
+zip -r ${PUBLISH_DIR}/dev-center.zip {.[^.],}*
 
 popd
 echo "DONE!"
