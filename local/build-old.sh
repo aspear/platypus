5e34a5410a3493d554b40698798f4b3848eec0c2
Aaron Spear <aspear@vmware.com>
2018-08-09 14:53:23 -0600

    working build that does require manual editing of the apis.json that is generated before working.

---
+++ local/build-old.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright VMware, Inc. 2017. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+#
+# You are free to use this file under the terms of the MIT open source license,
+# see LICENSE in the root of the containing project.
+#
+# DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+# EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+# NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This script builds an web server image in the file system of VMware API
+# Explorer (https://github.com/vmware/api-explorer), that is customized in this 
+# file with specific API references.  This image can then be served from any 
+# web container (tomcat, apache, NGINX, ...)
+#
+# VALUES THAT MUST BE CHANGED:
+# -At the bottom the apixlocal command run references string values specific
+# to the local APIs, these must be changed for every differnet package.
+# - the config.js file contains string and configuration values that are 
+# specific to a given set of APIs as well and should be customized.
+###############################################################################
+
+set -x  # fail the script if any command fails
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# -----------------------------------------------------------------------------
+# VARIABLES YOU CAN SET
+
+# you can supply an overridden build directory via BUILD_DIR variable if you 
+# wish.  If not provided, all output is created in this (the script) directory.
+BUILD_DIR=${BUILD_DIR:-${SCRIPT_DIR}}
+
+APIX_SERVER=https://apigw.vmware.com/v1/m4/api/dcr/rest/apix/apis
+
+# the VER variable is the one place to change the particular release of API 
+# explorer.  See https://github.com/vmware/api-explorer/releases for valid 
+# values
+export VER="2.0.0"
+export MILESTONE="a6"
+
+# -----------------------------------------------------------------------------
+APIX_RELEASE_URL=https://github.com/vmware/api-explorer/releases/download/${VER}${MILESTONE}
+
+OUTPUT_DIR=${BUILD_DIR}/staging
+OUTPUT_DIR_VA=${BUILD_DIR}/staging-va
+TOOLS_DIR=${BUILD_DIR}/tools
+DOWNLOAD_DIR=${BUILD_DIR}/download
+WAR_DIR=${BUILD_DIR}/war
+
+mkdir -p ${DOWNLOAD_DIR}
+
+# download zips of the distribution and tools if not cached locally
+if [ ! -f ${DOWNLOAD_DIR}/api-explorer-dist-${VER}.zip ]; then
+    wget --no-check-certificate ${APIX_RELEASE_URL}/api-explorer-dist-${VER}.zip --output-document ${DOWNLOAD_DIR}/api-explorer-dist-${VER}.zip
+fi
+
+if [ ! -f ${DOWNLOAD_DIR}/api-explorer-tools-${VER}.zip ]; then
+    wget --no-check-certificate ${APIX_RELEASE_URL}/api-explorer-tools-${VER}.zip --output-document ${DOWNLOAD_DIR}/api-explorer-tools-${VER}.zip
+    rm -rf ${TOOLS_DIR}  # if we downloaded new tools, wipe the old ones
+fi
+
+# only stage the tools once
+if [ -d "${TOOLS_DIR}" ]; then
+    echo "Already staged tools"
+else
+    echo "Staging tools"
+    mkdir -p ${TOOLS_DIR}
+    pushd ${TOOLS_DIR}
+    unzip ${DOWNLOAD_DIR}/api-explorer-tools-${VER}.zip
+    popd
+fi
+
+# Clean the staging directory if it already exists
+rm -rf ${OUTPUT_DIR_VA}/*
+mkdir -p ${OUTPUT_DIR_VA}
+
+rm -rf ${OUTPUT_DIR}/*
+mkdir -p ${OUTPUT_DIR}/local/swagger
+
+pushd ${OUTPUT_DIR}
+
+echo "Extracting APIX distribution"
+unzip ${DOWNLOAD_DIR}/api-explorer-dist-${VER}.zip
+
+echo "Overwriting stock apix-config.json with custom config"
+cp -f ${SCRIPT_DIR}/apix-config.json ./assets
+
+popd
+
+# run the tool to stage the swagger json files from the 
+# ${SCRIPT_DIR}/swagger directory to the local/swagger
+# directory, abbreviating the descriptions and then also taking the 
+# markdown description from the swagger and generating overview HTML
+# next to the json files.  When it does this it generates an "overview"
+# resource in the local.json file that has all of the configuration in it
+echo "staging local API content"
+python ${TOOLS_DIR}/apixlocal/apixlocal.py \
+ stage \
+ --server=${APIX_SERVER} \
+ --html_root_dir=${OUTPUT_DIR} \
+ --output_file=${OUTPUT_DIR}/local.json  \
+ --abbreviate_description \
+ --generate_overview_html \
+ --product_name="vRealize Automation;7.5" \
+ --api_version="(vRA 7.5)" \
+ --swagger_glob ${SCRIPT_DIR}/swagger/*.json \
+ --swagger_output_dir=${OUTPUT_DIR}/local/swagger 
+
+# inline replace title on the API Explorer index.html file to reflect our product branding
+sed -i 's/API Explorer/VMware vRealize Automation API Explorer/' ${OUTPUT_DIR}/index.html
+
+# now create a war file that is simply a wrapper on the image 
+#WAR_FILE_NAME=api-explorer-vra.war
+#mkdir -p ${OUTPUT_DIR}/WEB-INF
+#cat > ${OUTPUT_DIR}/WEB-INF/web.xml <<- "EOF"
+#<web-app version="3.0" xmlns="http://java.sun.com/xml/ns/javaee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd">
+#<display-name>VMware vRealize Automation API Explorer</display-name>
+#<welcome-file-list><welcome-file>index.html</welcome-file></welcome-file-list>
+#</web-app>
+#EOF
+#
+#zip -r ${OUTPUT_DIR}/${WAR_FILE_NAME} *
+
+echo "Creating VA staging dir"
+cp -R ${OUTPUT_DIR}/* ${OUTPUT_DIR_VA}
+find ${OUTPUT_DIR_VA} -type f -exec  ${SCRIPT_DIR}/adjustApiPathsForVra.sh {} \;
+
--- local/build.sh
+++ local/build.sh
@@ -2,31 +2,20 @@
 
 ###############################################################################
 # Copyright VMware, Inc. 2017. All Rights Reserved.
-# SPDX-License-Identifier: MIT
+# VMware internal, not open source.  (yet)
 #
-# You are free to use this file under the terms of the MIT open source license,
-# see LICENSE in the root of the containing project.
+# This script builds an web server image in the file system of the VMware {code}
+# "Devcenter App" https://gitlab.eng.vmware.com/gtix-tools/vcode-dev-center-app
+# 
+# Read https://gitlab.eng.vmware.com/gtix-tools/vcode-dev-center-app/blob/master/INTEGRATION.md'
+# for instructions on integration.
 #
-# DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
-# EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
-# WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
-# NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-#
-# This script builds an web server image in the file system of VMware API
-# Explorer (https://github.com/vmware/api-explorer), that is customized in this 
-# file with specific API references.  This image can then be served from any 
-# web container (tomcat, apache, NGINX, ...)
-#
-# VALUES THAT MUST BE CHANGED:
-# -At the bottom the apixlocal command run references string values specific
-# to the local APIs, these must be changed for every differnet package.
-# - the config.js file contains string and configuration values that are 
-# specific to a given set of APIs as well and should be customized.
 ###############################################################################
 
 set -x  # fail the script if any command fails
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+APIX_SERVER=https://apigw.vmware.com/v1/m4/api/dcr/rest/apix/apis
 
 # -----------------------------------------------------------------------------
 # VARIABLES YOU CAN SET
@@ -35,33 +24,35 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # wish.  If not provided, all output is created in this (the script) directory.
 BUILD_DIR=${BUILD_DIR:-${SCRIPT_DIR}}
 
-APIX_SERVER=https://apigw.vmware.com/v1/m4/api/dcr/rest/apix/apis
+# if your dev-center is not in the server root, you must supply is not in the root (likely)
+# you must supply this path here by setting DEV_CENTER_ROOT_PATH
+DEV_CENTER_ROOT_PATH=${DEV_CENTER_ROOT_PATH:-""}
 
 # the VER variable is the one place to change the particular release of API 
-# explorer.  See https://github.com/vmware/api-explorer/releases for valid 
-# values
-export VER="2.0.0"
-export MILESTONE="a6"
+# explorer.  See https://build-artifactory.eng.vmware.com/artifactory/npm/%40vmw/vcode-dev-center-app/-/@vmw
+# for valid values
+export VER="0.1.0"
 
 # -----------------------------------------------------------------------------
-APIX_RELEASE_URL=https://github.com/vmware/api-explorer/releases/download/${VER}${MILESTONE}
+ARTIFACTORY_URL=https://build-artifactory.eng.vmware.com/artifactory/npm/%40vmw/vcode-dev-center-app/-/@vmw
+DEV_CENTER_APP_FILE=vcode-dev-center-app-${VER}.tgz
 
 OUTPUT_DIR=${BUILD_DIR}/staging
 OUTPUT_DIR_VA=${BUILD_DIR}/staging-va
 TOOLS_DIR=${BUILD_DIR}/tools
 DOWNLOAD_DIR=${BUILD_DIR}/download
-WAR_DIR=${BUILD_DIR}/war
+PUBLISH_DIR=${BUILD_DIR}/publish
 
 mkdir -p ${DOWNLOAD_DIR}
 
-# download zips of the distribution and tools if not cached locally
-if [ ! -f ${DOWNLOAD_DIR}/api-explorer-dist-${VER}.zip ]; then
-    wget --no-check-certificate ${APIX_RELEASE_URL}/api-explorer-dist-${VER}.zip --output-document ${DOWNLOAD_DIR}/api-explorer-dist-${VER}.zip
-fi
-
-if [ ! -f ${DOWNLOAD_DIR}/api-explorer-tools-${VER}.zip ]; then
-    wget --no-check-certificate ${APIX_RELEASE_URL}/api-explorer-tools-${VER}.zip --output-document ${DOWNLOAD_DIR}/api-explorer-tools-${VER}.zip
+if [ ! -f ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE} ]; then
+    echo "Downloading archives of the distribution to ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE}"
+    wget --no-check-certificate ${ARTIFACTORY_URL}/${DEV_CENTER_APP_FILE} --output-document ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE}
+    pushd ${DOWNLOAD_DIR}
+    tar xfz ${DEV_CENTER_APP_FILE}  #this creates ${DOWNLOAD_DIR}/package/dist and ${DOWNLOAD_DIR}/package/tools
     rm -rf ${TOOLS_DIR}  # if we downloaded new tools, wipe the old ones
+else
+    echo "Using cached copy of ${DOWNLOAD_DIR}/${DEV_CENTER_APP_FILE}"
 fi
 
 # only stage the tools once
@@ -71,39 +62,47 @@ else
     echo "Staging tools"
     mkdir -p ${TOOLS_DIR}
     pushd ${TOOLS_DIR}
-    unzip ${DOWNLOAD_DIR}/api-explorer-tools-${VER}.zip
+    unzip ${DOWNLOAD_DIR}/package/tools/apixlocal.zip
     popd
 fi
 
 # Clean the staging directory if it already exists
-rm -rf ${OUTPUT_DIR_VA}/*
-mkdir -p ${OUTPUT_DIR_VA}
-
 rm -rf ${OUTPUT_DIR}/*
-mkdir -p ${OUTPUT_DIR}/local/swagger
-
+mkdir -p ${OUTPUT_DIR}
 pushd ${OUTPUT_DIR}
 
-echo "Extracting APIX distribution"
-unzip ${DOWNLOAD_DIR}/api-explorer-dist-${VER}.zip
+echo "Staging dev-center distribution"
+cp -R ${DOWNLOAD_DIR}/package/dist/{.[^.],}* .
 
-echo "Overwriting stock apix-config.json with custom config"
-cp -f ${SCRIPT_DIR}/apix-config.json ./assets
+echo "Cleaning up unneeded files in distribution"
+rm -fv ./assets/apix-swagger.json
+rm -fv ./assets/dev-center-overview.html
+rm -fv ./assets/sample-exchange-swagger.json
+#rm -fv ./assets/swagger-auth.json
+#rm -fv ./assets/swagger-complete.json
+rm -fv ./assets/vra-auth.json
+#rm -fv ./assets/vra-config.json
+rm -rfv ./local
 
-popd
+echo "Removing stock configuration files (will be overwritten, but to catch errors we remove them explicitly)"
+rm -fv ./assets/dev-center-config.json
 
+echo "Staging product specific dev-center-config.json as well as apis.json"
+cp -fv ${SCRIPT_DIR}/dev-center-config.json ./assets
+cp -fv ${SCRIPT_DIR}/favicon.ico ./    # copy VMware standard icon.  Replace with your products if there is one.
+
+echo "Generating apis.json"
 # run the tool to stage the swagger json files from the 
 # ${SCRIPT_DIR}/swagger directory to the local/swagger
 # directory, abbreviating the descriptions and then also taking the 
 # markdown description from the swagger and generating overview HTML
 # next to the json files.  When it does this it generates an "overview"
 # resource in the local.json file that has all of the configuration in it
-echo "staging local API content"
-python ${TOOLS_DIR}/apixlocal/apixlocal.py \
+python ${TOOLS_DIR}/apixlocal.py \
  stage \
  --server=${APIX_SERVER} \
  --html_root_dir=${OUTPUT_DIR} \
- --output_file=${OUTPUT_DIR}/local.json  \
+ --output_file=${OUTPUT_DIR}/apis.json  \
  --abbreviate_description \
  --generate_overview_html \
  --product_name="vRealize Automation;7.5" \
@@ -111,22 +110,22 @@ python ${TOOLS_DIR}/apixlocal/apixlocal.py \
  --swagger_glob ${SCRIPT_DIR}/swagger/*.json \
  --swagger_output_dir=${OUTPUT_DIR}/local/swagger 
 
-# inline replace title on the API Explorer index.html file to reflect our product branding
-sed -i 's/API Explorer/VMware vRealize Automation API Explorer/' ${OUTPUT_DIR}/index.html
+echo "Using DEV_CENTER_ROOT_PATH variable to set base path in generated files to \"${DEV_CENTER_ROOT_PATH}\""
+sed -i "s|<base href=\\\"/\\\">|<base href=\\\"/${DEV_CENTER_ROOT_PATH}\\\">|g" ./index.html
+sed -i "s|/dev-center-app/|/${DEV_CENTER_ROOT_PATH}/|g" ./.htaccess
+sed -i "s|/dev-center-app/|/${DEV_CENTER_ROOT_PATH}/|g" ./server.rewrites
 
-# now create a war file that is simply a wrapper on the image 
-#WAR_FILE_NAME=api-explorer-vra.war
-#mkdir -p ${OUTPUT_DIR}/WEB-INF
-#cat > ${OUTPUT_DIR}/WEB-INF/web.xml <<- "EOF"
-#<web-app version="3.0" xmlns="http://java.sun.com/xml/ns/javaee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd">
-#<display-name>VMware vRealize Automation API Explorer</display-name>
-#<welcome-file-list><welcome-file>index.html</welcome-file></welcome-file-list>
-#</web-app>
-#EOF
-#
-#zip -r ${OUTPUT_DIR}/${WAR_FILE_NAME} *
+echo "Publishing output zip file ${PUBLISH_DIR}/dev-center.zip"
+mkdir -p ${PUBLISH_DIR}
+zip -r ${PUBLISH_DIR}/dev-center.zip {.[^.],}*
 
+
+# Clean the staging directory if it already exists
 echo "Creating VA staging dir"
+rm -rf ${OUTPUT_DIR_VA}/*
+mkdir -p ${OUTPUT_DIR_VA}
 cp -R ${OUTPUT_DIR}/* ${OUTPUT_DIR_VA}
 find ${OUTPUT_DIR_VA} -type f -exec  ${SCRIPT_DIR}/adjustApiPathsForVra.sh {} \;
 
+popd
+echo "DONE!"
